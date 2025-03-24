55d39152332851e506b697b627d45775bad74ce4
bruna <bruna@teste.com>
2025-03-24 16:02:45 -0300

    test: mudanças em teste

--- src/script/script.js
+++ src/script/script.js
@@ -2,7 +2,7 @@ document.getElementById("whatsappLink").addEventListener("click", function (e) {
     e.preventDefault() // Previne o comportamento padrão do link
     let numero = "5521975956657" 
     let mensagem = "Oi, gostaria de saber mais sobre os seus serviços!" // Mensagem predefinida
-    let url = "https://wa.me/" + numero + "?text=" + encodeURIComponent(mensagem)
+    const url = `https://wa.me/${numero}?text=${encodeURIComponent(mensagem)}`
 
   window.open(url, "_blank") // Abre o link do WhatsApp em uma nova aba
 })
